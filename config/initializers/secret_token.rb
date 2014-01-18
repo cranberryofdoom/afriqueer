0e17e9eea46d498209d4078233ba9fc785dbb75e
Ashley Qian <ashley.qian@duke.edu>
2014-01-17 20:01:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Afriqueer::Application.config.secret_key_base = 'c0530758e17a637e1b12f1857c8579dfc1b760f92b2e8c7a87402f22d875c2efe3c5be04aaf14164291007b34b01c4d1f2ac742077fa4e228b6f603bcce8b0d9'
